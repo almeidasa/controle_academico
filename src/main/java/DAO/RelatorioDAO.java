eec5498bb946b57f8a689b61195a3c5281b886cf
ALEXANDRE-CASA\Alexandre Almeida <email@email.com.br>
2018-11-25 13:27:29 -0200

    ColarGrau

--- src/main/java/DAO/RelatorioDAO.java
+++ src/main/java/DAO/RelatorioDAO.java
@@ -60,6 +60,49 @@ public class RelatorioDAO {
         return historico;
     }
 
+//    public ArrayList<HistoricoAluno> obterPodemColarGrau(int codCurso) {
+//
+//        ArrayList<HistoricoAluno> historico = new ArrayList<>();
+//
+//        String SQL = "SELECT c.nome_curso, a.nome AS nomealuno, mc.matricula, d.nome AS nomedisciplina, md.ano, md.semestre, md.conceito FROM aluno a\n"
+//                + "INNER JOIN MatriculaCurso mc on(mc.fk_Aluno_cpf = a.cpf)\n"
+//                + "INNER JOIN Curso c ON(c.cod = mc.fk_Curso_cod)\n"
+//                + "INNER JOIN MatriculaDisciplina md on(md.fk_Aluno_cpf = a.cpf)\n"
+//                + "INNER JOIN Disciplina d ON(d.codigo = md.fk_Disciplina_codigo AND d.fk_Curso_cod = ?)\n"
+//                + "WHERE a.cpf = ? AND mc.fk_Curso_cod = ? ORDER BY md.ano, md.semestre";
+//        try (PreparedStatement pstm = BD.getConexao().prepareStatement(SQL)) {
+//            pstm.setInt(1, codCurso);
+//            pstm.setString(2, cpf);
+//            pstm.setInt(3, codCurso);
+//
+//            System.out.println(cpf);
+//            System.out.println(codCurso);
+//
+//            ResultSet rs = pstm.executeQuery();
+//            while (rs.next()) {
+//                HistoricoAluno ha = new HistoricoAluno(
+//                        rs.getString("nome_curso"),
+//                        rs.getString("nomealuno"),
+//                        cpf,
+//                        rs.getString("matricula"),
+//                        rs.getString("nomedisciplina"),
+//                        rs.getString("ano"),
+//                        rs.getString("semestre"),
+//                        rs.getString("conceito") == null || rs.getString("conceito").equals("") ? "-" : rs.getString("conceito"),
+//                        rs.getString("conceito") != null &&  !rs.getString("conceito").equals("") ? (rs.getString("conceito").equals("Insuficiente") ? "Reprovado" : "Aprovado") : "M"
+//                );
+//                historico.add(ha);
+//            }
+//            pstm.close();
+//            Exibir.Mensagem("Erro ao obter Histórico!");
+//            System.out.println("Histórico obtido com sucesso!");
+//        } catch (Exception ex) {
+//            System.out.println("Erro" + ex);
+//            Exibir.Mensagem("Erro ao obter Histórico!: \n" + ex);
+//        }
+//        return historico;
+//    }
+    
     public void obterFoto(String cpf) {
         String SQL = "SELECT bin_foto FROM aluno WHERE cpf = (?)";
 
---
+++ src/main/java/controller/PodemColarGrauBean.java
@@ -0,0 +1,128 @@
+package controller;
+
+import DAO.AlunoDAO;
+import DAO.CursoDAO;
+import DAO.RelatorioDAO;
+import Util.Relatorio;
+import entities.Aluno;
+import entities.Curso;
+import entities.HistoricoAluno;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * @Autor Winder Rezende
+ * @Data 20/11/2018
+ */
+@ManagedBean
+@ViewScoped
+public class PodemColarGrauBean {
+
+    private int codCurso;
+    private String alunoCpf;
+    private ArrayList<Aluno> alunos;
+    private ArrayList<Curso> curso;
+    private ArrayList<HistoricoAluno> historico;
+    private Map<String, String> ItensBoxAlunos;
+    private Map<Integer, String> ItensBoxCurso;
+
+    public PodemColarGrauBean() {
+        this.alunos = new ArrayList<>();
+        this.curso = new ArrayList<>();
+        setBoxCurso();
+    }
+    
+    private void setBoxCurso() {
+        ItensBoxCurso = new LinkedHashMap<>();
+        CursoDAO c = new CursoDAO();
+        curso = c.obterCursos();
+
+        for (Curso cursos : curso) {
+            ItensBoxCurso.put(0, "Selecione um Curso");
+            ItensBoxCurso.put(cursos.getCod(), cursos.getNome_curso());
+        }
+    }
+    
+    public void setBoxAlunos() {
+        ItensBoxAlunos = new LinkedHashMap<>();
+        AlunoDAO al = new AlunoDAO();
+        alunos = al.obterAlunosCurso(codCurso);
+
+        for (Aluno aluno : alunos) {
+            ItensBoxAlunos.put("A", "Selecione um Aluno");
+            ItensBoxAlunos.put(aluno.getCpf(), aluno.getNome());
+        }
+    }
+    
+    public void obter(){
+        this.historico = new ArrayList<>();
+        RelatorioDAO rel = new RelatorioDAO();
+        historico = rel.obterHistorico(alunoCpf, codCurso);
+        rel.obterFoto(alunoCpf);
+    }
+    
+    public void gerarRelatorio(){
+        Relatorio gerar = new Relatorio();
+        gerar.getHistoricoAluno(historico);
+    }
+    
+    //Getters e Seters
+    public int getCodCurso() {
+        return codCurso;
+    }
+
+    public void setCodCurso(int codCurso) {
+        this.codCurso = codCurso;
+    }
+
+    public String getAlunoCpf() {
+        return alunoCpf;
+    }
+
+    public void setAlunoCpf(String alunoCpf) {
+        this.alunoCpf = alunoCpf;
+    }
+
+    public ArrayList<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(ArrayList<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    public ArrayList<Curso> getCurso() {
+        return curso;
+    }
+
+    public void setCurso(ArrayList<Curso> curso) {
+        this.curso = curso;
+    }
+
+    public ArrayList<HistoricoAluno> getHistorico() {
+        return historico;
+    }
+
+    public void setHistorico(ArrayList<HistoricoAluno> historico) {
+        this.historico = historico;
+    }
+
+    public Map<String, String> getItensBoxAlunos() {
+        return ItensBoxAlunos;
+    }
+
+    public void setItensBoxAlunos(Map<String, String> ItensBoxAlunos) {
+        this.ItensBoxAlunos = ItensBoxAlunos;
+    }
+
+    public Map<Integer, String> getItensBoxCurso() {
+        return ItensBoxCurso;
+    }
+
+    public void setItensBoxCurso(Map<Integer, String> ItensBoxCurso) {
+        this.ItensBoxCurso = ItensBoxCurso;
+    }
+}
